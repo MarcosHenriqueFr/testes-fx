4b4c43ea534762fb2106977da41b9ac3c980e354
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-04-23 11:11:00 -0300

    Update Layout

--- exercicio-javafx/src/main/java/br/com/empresa/app/fxml/AppFXML.java
+++ exercicio-javafx/src/main/java/br/com/empresa/app/fxml/AppFXML.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -16,9 +17,9 @@ public class AppFXML extends Application {
         // Os processos são semelhantes, e ambos, CSS e FXML tem que ser carregados no meu start
         String fileCSS = getClass().getResource("/br/com/empresa/app/fxml/LoginStyle.css").toExternalForm();
         URL fileFXML = getClass().getResource("/br/com/empresa/app/fxml/Login.fxml");
-        GridPane root = FXMLLoader.load(fileFXML);
 
-        Scene scene = new Scene(root, 350, 400);
+        GridPane root = FXMLLoader.load(fileFXML);
+        Scene scene = new Scene(root, 350, 350);
         scene.getStylesheets().add(fileCSS);
 
         stage.setResizable(false); // Não permite a alteração do tamanho
